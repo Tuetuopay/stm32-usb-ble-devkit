2601c1515c35c093a5bfc83f0ec7cf8190e979e2
Tuetuopay <tuetuopay@me.com>
2018-12-16 00:45:47 +0100

    stm32: Make logger thread safe

--- firmware/stm32/src/logger.c
+++ firmware/stm32/src/logger.c
@@ -14,6 +14,9 @@
 extern SerialDriver SD1;
 BaseSequentialStream * const chout = (BaseSequentialStream*)&SD1;
 
+// Mutex to avoid mixing logs of separate threads
+static mutex_t mutex;
+
 void loggerInit(void) {
 	// Configure pins
 	palSetLineMode(LINE_TX, PAL_MODE_ALTERNATE(7U));
@@ -22,26 +25,19 @@ void loggerInit(void) {
 	// Start serial driver
 	static SerialConfig serialConfig = {115200, 0, 0, 0};
 	sdStart(&SD1, &serialConfig);
+
+	chMtxObjectInit(&mutex);
 }
 
 void loggerHeader(void) {
-	int now = stGetCounter(), n = 0;
+	int now = stGetCounter();
+	chprintf(chout, "[" YELLOW "%7d" RST "] ", now);
+}
 
-	chprintf(chout, "[" YELLOW);
-	if (now < 10)
-		n = 6;
-	else if (now < 100)
-		n = 5;
-	else if (now < 1000)
-		n = 4;
-	else if (now < 10000)
-		n = 3;
-	else if (now < 100000)
-		n = 2;
-	else if (now < 1000000)
-		n = 1;
+void loggerLock(void) {
+	chMtxLock(&mutex);
+}
 
-	for (int i = 0; i < n; i++)
-		chprintf(chout, " ");
-	chprintf(chout, "%d" RST "] ", now);
+void loggerUnlock(void) {
+	chMtxUnlock(&mutex);
 }
--- firmware/stm32/src/logger.h
+++ firmware/stm32/src/logger.h
@@ -17,31 +17,47 @@ void loggerInit(void);
 void loggerHeader(void);
 
 /**
- * Logs to chout without any additions.
+ * Locks the logger mutex to avoid mixing the output of multiple threads.
+ */
+void loggerLock(void);
+
+/**
+ * Unlocks the logger mutex to avoid mixing the output of multiple threads.
+ */
+void loggerUnlock(void);
+
+/**
+ * Logs to chout without any additions. Not thread safe.
  * @param format   printf-like formatted string
  * @param args     args for the formatted string
  */
+void rlow(const char *format, ...);
 #define rlog(format, args...) do { chprintf(chout, format, ## args); } while(0);
 
 /**
- * Logs to chout with a header containing the current system time.
+ * Logs to chout with a header containing the current system time. Thread safe
  * @param format   printf-like formatted string
  * @param args     args for the formatted string
  */
 #define log(format, args...) do { \
+	loggerLock(); \
 	loggerHeader(); \
 	rlog(format, ## args); \
+	loggerUnlock(); \
 } while(0);
 
 /**
  * Logs to chout with a header containing the current system time and an
- * included line return (CRLF).
+ * included line return (CRLF). Thread safe.
  * @param format   printf-like formatted string
  * @param args     args for the formatted string
  */
 #define lognl(format, args...) do { \
-	log(format, ## args); \
+	loggerLock(); \
+	loggerHeader(); \
+	rlog(format, ## args); \
 	chprintf(chout, "\r\n"); \
+	loggerUnlock(); \
 } while(0);
 
 #endif
